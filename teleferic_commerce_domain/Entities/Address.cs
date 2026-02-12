3551861e1c8d508c732ae5a9d726007b9a0b42bd
Hakan Bozkurt <tufoyna@gmail.com>
2026-02-12 22:41:32 +0300

    Address sınıfı güncellendi, Identity desteği eklendi
    Address sınıfı BaseEntity'den türetildi ve yeni özellikler
    eklendi (UserId, FullName, PhoneNumber, vb.).
    
    Projeye ASP.NET Core Identity ve Entity Framework Core
    için gerekli NuGet paketleri eklendi.
    
    Yeni bir ApplicationUser sınıfı oluşturularak IdentityUser
    sınıfından türetildi.

--- teleferic_commerce_domain/Entities/Address.cs
+++ teleferic_commerce_domain/Entities/Address.cs
@@ -7,7 +7,16 @@ using System.Threading.Tasks;
 namespace teleferic_commerce_domain.Entities
 {
 
-    public class Address
+    public class Address : BaseEntity
     {
+        public string UserId { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string District { get; set; } = string.Empty;
+        public string Addressline{ get; set; } = string.Empty;
+        public string Zipcode { get; set; } 
+
+        public string IsDefault { get; set; } 
     }
 }
